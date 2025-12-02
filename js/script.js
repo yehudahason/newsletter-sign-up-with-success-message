29055c5ee1bec918dc762f28b9756ef72b21d48a
Yehuda Hason <yehudaxt@gmail.com>
2025-12-02 19:31:31 +0200

    regex commit

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ const email = document.getElementById("email");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(email.value);
+  const result = emailIsValid(email.value);
+
+  if (result) {
+    console.log(result);
+  } else {
+    console.log("bad email");
+  }
 });
+
+function emailIsValid(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
